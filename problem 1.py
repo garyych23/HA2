c7a7f8a4d2063f139a5d62584abed1e1c6f715dc
AlexGruel <alex06.gruel@gmail.com>
2024-05-11 16:01:53 +0200

    presque bon pb1

--- problem 1.py
+++ problem 1.py
@@ -13,7 +13,7 @@ n = len(tau)
 N = 1000 # MC size
 d = 2 # Number of break points - 1
 nu = 1/5 # Hyperparameter of teta
-rho = 5
+rho = 1/2
 
 def countau(tau,t1,t2):
     count = 0
@@ -27,6 +27,7 @@ theta = np.random.gamma(2, nu)
 lambda_list = np.random.gamma(2, theta, d)
 t_list = np.linspace(1851, 1963, d+1) 
 for k in range(1,N):
+    print("Current k=", k)
     # Gibbs : teta, lambda
     theta = np.random.gamma(2, np.sum(lambda_list) + nu)
     for i in range(d):
@@ -34,18 +35,21 @@ for k in range(1,N):
         lambda_list[i] = np.random.gamma(n_i + 2, t_list[i+1] - t_list[i] + theta)
     # Metropolis-Hastings with Random walk proposal: t
     diff = np.diff(t_list)
-    while not np.any(diff <= 0): # 
-        for i in range(1,d): # first and last time points are fixed
-            R = rho*(t_list[i+1] - t_list[i-1])
-            eps = np.random.rand()*2*R - R
-            t_star = t_list[i] + eps
-            ratio = ((t_list[i+1]-t_star)*(t_star-t_list[i-1]))/((t_list[i+1]-t_list[i])*(t_list[i]-t_list[i-1]))\
-            * np.exp((lambda_list[i]-lambda_list[i-1])*(t_star-t_list[i]))\
-            * lambda_list[i-1]**(countau(tau,t_list[i-1],t_star)-countau(tau,t_list[i-1],t_list[i]))\
-            * lambda_list[i]**(countau(tau,t_star,t_list[i+1])-countau(tau,t_list[i],t_list[i+1]))
-            alpha = np.min([1,ratio])
-            U = np.random.rand()
-            if U <= alpha:
-                t_list[i] = t_star
+    i=1
+    while (not np.any(diff <= 0)) and (i < d): # 
+        #for i in range(1,d): # first and last time points are fixed
+        R = rho*(t_list[i+1] - t_list[i-1])
+        eps = np.random.rand()*2*R - R
+        t_star = t_list[i] + eps
+        print("t_star:", t_star)
+        ratio = ((t_list[i+1]-t_star)*(t_star-t_list[i-1]))/((t_list[i+1]-t_list[i])*(t_list[i]-t_list[i-1]))\
+        * np.exp((lambda_list[i]-lambda_list[i-1])*(t_star-t_list[i]))\
+        * lambda_list[i-1]**(countau(tau,t_list[i-1],t_star)-countau(tau,t_list[i-1],t_list[i]))\
+        * lambda_list[i]**(countau(tau,t_star,t_list[i+1])-countau(tau,t_list[i],t_list[i+1]))
+        alpha = np.min([1,ratio])
+        U = np.random.rand()
+        if U <= alpha:
+            t_list[i] = t_star
+        i += 1
         
         
